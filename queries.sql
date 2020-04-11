106a01b629c9c50ec13df2d2120abdd10c81edcf
Eddie Jimenez <52768061+l8nightswithJS@users.noreply.github.com>
2020-04-10 19:34:59 -0700

    MVP New

--- queries.sql
+++ queries.sql
@@ -19,7 +19,7 @@
 -- Find all customers that live in London, Madrid, or Brazil
     SELECT *
     FROM Customers
-    WHERE City = 'London' OR Country = 'Brazil' OR City = 'Madrid';
+    WHERE City IN ('London', 'Madrid') OR Country = 'Brazil';
 -- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
     INSERT INTO Customers (CustomerName, ContactName, Address, City, PostalCode, Country)
     VALUES ('The Shire', 'Bilbo Baggins', '1 Hobbit-Hole', 'Bag End', '111', 'Middle Earth')
